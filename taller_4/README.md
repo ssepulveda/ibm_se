ab3383ed83f8a52bdc1513b60ac6f5252b2cb7ca
ssepulveda <ssepulveda.sm@gmail.com>
2013-08-27 21:25:04 -0400

    adds documentation for doxygen

--- taller_4/README.md
+++ taller_4/README.md
@@ -1,6 +1,14 @@
-Taller 4
+Taller 4 Sistemas Embebidos
+Departamento Ingenieria Biomedica, Universidad de Valparaiso
 ====
 
-Firmware para ATmega32u4 de Arduino Leonardo.
+Comunicación serial por medio de LUFA y USART utilizando Arduino Leonardo como decodificador entre el [módulo SPO2 SP100](http://www.goldwei.com/products/item.asp?itemid=391) (PN: MO-SP100) y un computador.
 
-Decodificador para Oximetro de Pulso.
+Incluye código de firmware para Arduino Leonardo y una aplicación para visualizar el estado del oxímetro de pulso y graficar los datos en tiempo real.
+
+
+
+
+
+
+
--- taller_4/main.c
+++ taller_4/main.c
@@ -1,28 +1,52 @@
 /******************************************************************************
- * Main.c
- * 
- * Autor: Sebastian Sepulveda
- * Licencia: New BSD 
- * Descripcion:
- * Arduino Leonardo + USART + LUFA
- * Sources: pp 196 datasheet at32u4
- *
- ******************************************************************************/
+* Taller 4 Sistemas Embebidos
+* Departamento Ingenieria Biomedica, Universidad de Valparaiso
+*
+* Autor: Sebastián Sepúlveda
+* Licencia: New BSD
+* Descripcion:
+* Comunicación serial por medio de LUFA y USART utilizando Arduino Leonardo
+* como decodificador entre el módulo SPO2 SP100 (PN: MO-SP100) y un computador
+*
+******************************************************************************/
+
 #include <avr/io.h>
 #include "serial.h"
 
-/* Function prototype*/
+// Function protoype
 void LUFA_Init(void);
 void USART_Init(unsigned int baud);
 unsigned char USART_Receive(void);
-void USART_Transmit(unsigned char data);
-void USART_Flush(void);
+void getData(void);
+void sendData(void);
+void serial_writeBin(uint8_t bin);
 
-/* Global variables */
+/** Bytes obtenidos desde el oxímetro de pulso
+ * @details Guarda los datos obtenidos desde el oxímetro de pulso tras la 
+ * última adquisición mediante @ref getData
+ */
 volatile char data[5];
-volatile char bin2hex[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+/** Arreglo de conversión de binario a Hexadecimal
+ * @details El arreglo permite la conversión de datos binarios a su
+ * representación hexadecimal, listando los valores que adquiere para cada
+ * valor. Utilizando por la función @ref serial_writeBin.
+ */
+volatile char bin2hex[16]={'0','1','2','3','4','5','6','7',
+                           '8','9','A','B','C','D','E','F'};
 
-/* Main */
+/** Funcion principal de la aplicación
+ * @details Se ejecuta al inicial el procesamiento del microcontrolador
+ * 
+ * Inicializa los puertos y protocolos a usar
+ * mediante @ref USART_Init para comunicación entre el oxímetro de pulso
+ * con el microcontrolador mediante una conexión serial 8N1 a 4800 bps y 
+ * @LUFA_Init para la comunicación entre el microcontrolador y un computador, 
+ * mediante una conexión serial 8N1 a 115200 bps.
+ * 
+ * Posteriormente, solicita datos al oxímetro de pulso mediante @ref getData
+ * y los envía al computador decodificados mediante @sendData, repitiendo el
+ * proceso hasta que se desactive el microcontrolador.
+ */
 int main(void){
     USART_Init(207);
     LUFA_Init();
@@ -35,7 +59,16 @@ int main(void){
     return 0;
 }
 
-/* decode funtions */
+/** Adquiere datos desde el oxímetro de pulso
+ * @details Según la hoja de información del oxímetro de pulso,
+ * los datos se transmiten en paquetes de datos de 5 bytes.
+ *
+ * La función espera a que el oxímetro de pulso envíe un SYNC bit, detectando
+ * el inicio de la transmición del paquete de datos. Comprueba además que los 
+ * datos recibidos corresponden al paquete al analizar el primer bit de cada
+ * byte despúes del SYNC bit.
+ * @return Retorna los paquetes de datos al arreglo @ref data 
+ */
 void getData(void){
     unsigned char tmp;
     int sync=0;
@@ -59,6 +92,17 @@ void getData(void){
     }
 }
 
+/** Decodifica y envía los datos del oxímetro de pulso al computador
+ * @details Lee los datos desde el arreglo @ref data y decodifica los valores
+ * según las especificaciones del fabricante del oxímetro de pulso.
+ *
+ * Decodifica tanto valores numéricos como de estado (Binarios), para luego
+ * enviarlos en formato CSV al computador.
+ * Los datos llevan la estructura:
+ *
+ * @code pulseIntensity,oxygenDissolved,pulseSound,pulseRate,oxygenSaturation,
+ searchTimeTooLong,oxygenSaturationDecrease,probeError,searchPulse @endcode
+ */
 void sendData(void){
     // numeric data
     uint8_t pulseIntesity=data[0]&4;
@@ -108,18 +152,32 @@ void sendData(void){
     serial_write('\r');
 }
 
+/** Convierte datos binarios a su representación Hexadecimal
+ * @details Utiliza el arreglo @ref bin2hex para obtener la reprsentación
+ * numerica hexadecimal de un número binario de 8 bits.
+ * @param bin valor binario a convertir
+ */
 void serial_writeBin(uint8_t bin){
     serial_write(bin2hex[(bin&0xF0)>>4]);
     serial_write(bin2hex[bin&0x0F]);
 }
 
-/* LUFA functions */
+/** Inicializa el puerto serial para la comunicación con el computador
+ * @details Inicializa la librería LUFA para administrar la comunicación
+ * serial entre el microcontrolador y el computador
+ */
 void LUFA_Init(void){
     // Enable LUFA
     serial_init();
 }
 
-/* USART functions */
+/** Inicializa el puerto USART para la comunicación con el oxímetro de pulso
+ * @details Configura el puerto USART para ser utilizado como puerto serial
+ * 8N1, habilitando los registros necesarios
+ * @param baud velocidad del puerto serial, obtenido por la tabla 18-12 del
+ * datasheet del microcontrolador ATMega 32u4
+ * @note Sólo se habilita la recepción de datos, no la transmición
+ */
 void USART_Init(unsigned int baud){
     // Set baud rate (from table 18-12 pp213)
     UBRR1H=(unsigned char)(baud>>8);
@@ -140,25 +198,14 @@ void USART_Init(unsigned int baud){
     UCSR1B=(1<<RXEN1);
 }
 
+/** Recibe datos desde el oxímetro de pulso
+ * @details Espera la recepción de un byte completo y la retorna
+ * @return último byte recibido
+ */
 unsigned char USART_Receive(void){
     // Wait for data to be received
     while(!(UCSR1A & (1<<RXC1)));
 
     // Get and return received data from buffer
     return UDR1;
-}
-
-void USART_Transmit(unsigned char data){
-    // Wait for empty transmit buffer
-    while(!( UCSR1A & (1<<UDRE1)));
-
-    // Put data into buffer, sends the data
-    UDR1 = data;
-}
-
-void USART_Flush(void){
-    unsigned char dummy;
-    while(UCSR1A & (1<<RXC1)){
-        dummy = UDR1;
-    }
 }
